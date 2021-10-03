d4f0a7ae68e84d39304a777b04213a91cefcb208
waelarous <waelarous2009@gmail.com>
2021-10-03 17:07:37 +0100

    List provider

---
+++ src/main/java/com/sip/ams/AmsMvcdataApplication.java
@@ -0,0 +1,13 @@
+package com.sip.ams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmsMvcdataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmsMvcdataApplication.class, args);
+	}
+
+}
